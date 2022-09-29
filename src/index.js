b5d4887371a40be31a5bcbd16e5b518e33b64d65
Kevin Sasse <sassek70@gmail.com>
2022-09-29 09:49:29 -0400

    finished lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,92 @@
-// write your code here!
+
+let theChosenOne;
+
+fetch(`http://localhost:3000/ducks`)
+    .then(res => res.json())
+    .then(ducksArray => {
+        renderDuckImages(ducksArray)
+        duckDetails(ducksArray[0])})
+
+const renderDuckImages = (ducksArray) => {
+    ducksArray.forEach(duck => createImage(duck))
+};
+
+const createImage = (duck) => {
+    const navBar = document.getElementById('duck-nav')
+    const duckImg = document.createElement('img')
+
+    duckImg.src = duck.img_url
+
+    navBar.appendChild(duckImg)
+
+    duckImg.addEventListener('click', (e) =>
+    duckDetails(duck))
+
+}
+
+const duckDetails = (duck) => {
+    const duckName = document.getElementById('duck-display-name')
+    const duckDisplayImg = document.getElementById('duck-display-image')
+    const duckLikes = document.getElementById('duck-display-likes')
+
+    duckName.textContent = duck.name
+    duckDisplayImg.src = duck.img_url
+    duckLikes.textContent = duck.likes + " likes"
+
+    theChosenOne = duck
+}
+
+   const updateLikesBtn = document.getElementById('duck-display-likes')
+    updateLikesBtn.addEventListener('click', (e) => {
+        console.log(theChosenOne.likes)
+        updateLikesBtn.textContent = `${++theChosenOne.likes} likes`
+})
+
+
+
+const createDuckForm = () => {
+    const form = document.getElementById('new-duck-form')
+    const newName = document.getElementsByName('duck-name-input')
+    const newImage = document.getElementsByName('duck-image-input')
+    
+    const newDuckObj = {
+        name: newName.textContent,
+        img_url: newImage.textContent,
+        Likes: 0
+    }
+    
+    createImage(newDuckObj)
+    form.reset()
+    
+}
+
+const createButton = document.getElementsByTagName('input')[2]
+    createButton.addEventListener('click', (e) => {
+        e.preventDefault()
+        createDuckForm()
+    })
+
+
+
+
+/*
+
+    Challenge 1:
+        - Display all ducks in the bar - http://localhost:3000/ducks - complete
+
+    Challenge 2:
+        - Display details a duck when clicked along with a "likes" button and number of likes - complete
+
+    Challenge 3:
+        - Like button increments total likes of the specific duck.
+            - "X likes"
+
+    Challenge 4:
+        - Create new duck with same behaviors as Challenges 2 & 3. Starts with 0 likes.
+            - Persistance not necessary
+
+
+
+
+
+*/
